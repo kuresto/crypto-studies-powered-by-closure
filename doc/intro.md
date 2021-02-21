45bb019aa152299f2b23301f140415e759e975fb
Nilton Teixeira <9078708+kuresto@users.noreply.github.com>
2021-02-21 16:16:25 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crypto-studies-powered-by-closure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/crypto_studies_powered_by_closure/core.clj
@@ -0,0 +1,7 @@
+(ns crypto-studies-powered-by-closure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
